610ce73596e6ab7a288145cb1be809eb47497370
felipebn <thrash.nation@gmail.com>
2019-03-02 22:45:32 +0000

    Graphiql fix (missing dep) and adding new query (nextBudget)

--- src/main/kotlin/devlium/budgetr/BudgetrApplication.kt
+++ src/main/kotlin/devlium/budgetr/BudgetrApplication.kt
@@ -1,31 +1,20 @@
 package devlium.budgetr
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.*
 
 @SpringBootApplication
 class BudgetrApplication{
 
-	/*@Bean
+	@Bean
 	fun init(seedLoader : Optional<SeedDataLoader>, budgetService :BudgetServiceBean) = CommandLineRunner{
 		seedLoader.ifPresent {
 			it.seedData()
 		}
-		
-		val start = LocalDate.of(2019, Month.FEBRUARY, 24)
-		val period = Period(start, start.plusDays(6))
-
-		val forecast = budgetService.forecast(period, 8)
-		
-		forecast.budgets.forEach { budget ->
-			println("Week budget (${budget.period}): ${budget.total}")
-			budget.expenses.forEach{
-				println("${it.amount}\t${it.description}")
-			}	
-		}
-		
-		println("Total Forecast: ${forecast.total}")
-	}*/
+	}
 	
 }
 
--- src/main/kotlin/devlium/budgetr/api/BudgetResolver.kt
+++ src/main/kotlin/devlium/budgetr/api/BudgetResolver.kt
@@ -1,6 +1,7 @@
 package devlium.budgetr.api
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
+import devlium.budgetr.Budget
 import devlium.budgetr.BudgetServiceBean
 import devlium.budgetr.Period
 import org.springframework.stereotype.Component
@@ -14,20 +15,13 @@ class BudgetResolver(
     fun currentBudget() : ResolvedBudget{
         val period = Period.fromWeekStart(LocalDate.now())
         val budget = budgetServiceBean.project(period)
+        return ResolvedBudget(budget)
+    }
 
-        return ResolvedBudget(
-                start = budget.period.start,
-                end = budget.period.end,
-                total = budget.total,
-                expenses = budget.expenses.map{
-                    ResolvedExpense(
-                            description = it.description,
-                            plannedDate = it.applicableDateInPeriod(period),
-                            amount = it.amount
-                    )
-                }
-
-        )
+    fun nextBudget() : ResolvedBudget{
+        val period = Period.fromWeekStart(LocalDate.now().plusDays(7))
+        val budget = budgetServiceBean.project(period)
+        return ResolvedBudget(budget)
     }
 
 }
@@ -37,7 +31,20 @@ data class ResolvedBudget(
         val end:LocalDate,
         val total:Double,
         val expenses:List<ResolvedExpense>
-)
+){
+    constructor(budget:Budget) : this(
+        start = budget.period.start,
+        end = budget.period.end,
+        total = budget.total,
+        expenses = budget.expenses.map{
+            ResolvedExpense(
+                description = it.description,
+                plannedDate = it.applicableDateInPeriod(budget.period),
+                amount = it.amount
+            )
+        }
+    )
+}
 
 data class ResolvedExpense(
         val description: String,
